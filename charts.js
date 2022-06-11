78388c342e47d8927f54992948b49b671825714f
yvoatelep <yvoatelep@gmail.com>
2022-06-11 12:41:13 -0600

    for learning assist

--- charts.js
+++ charts.js
@@ -101,3 +101,28 @@ function buildCharts(sample) {
   });
 }
 
+ // 1. Create the trace for the bubble chart.
+ var bubbleData = {
+  x: [otuids.map (otuid=> "OTU " + otuid).slice(0,10)],
+  y: [sampleValues],
+  text: [otulabels],
+  mode: 'markers',
+  marker: {
+    color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+    size: [40, 60, 80, 100]
+  }
+};
+
+var data = [bubbleData];
+
+// 2. Create the layout for the bubble chart.
+var layout = {
+  title: 'Bacteria Cultures Per Sample',
+  showlegend: false,
+  height: 600,
+  width: 600
+};
+
+// 3. Use Plotly to plot the data with the layout.
+Plotly.newPlot('bubble', data, layout);
+
